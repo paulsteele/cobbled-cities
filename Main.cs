793b35c5628ec54021aa5a28bc9aa843de83bbf1
Paul Steele <paul@paul-steele.com>
2023-04-08 18:49:43 -0400

    update main to program

--- Main.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace schematic_to_lost_cities;
-
-public class Main
-{
-}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace schematic_to_lost_cities;
+
+// ReSharper disable once UnusedType.Global
+public class Program
+{
+	// ReSharper disable once UnusedMember.Global
+	public static void Main()
+	{
+		Console.WriteLine("test");
+	}
+}
