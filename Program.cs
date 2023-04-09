a569a4a9a0900eff9e9cd03f979b6845360b8d87
Paul Steele <paul@paul-steele.com>
2023-04-08 22:17:28 -0400

    read in schematic

--- Program.cs
+++ Program.cs
@@ -1,11 +1,14 @@
-﻿namespace schematic_to_lost_cities;
+﻿using fNbt;
+
+namespace schematic_to_lost_cities;
 
 // ReSharper disable once UnusedType.Global
-public class Program
+public static class Program
 {
 	// ReSharper disable once UnusedMember.Global
 	public static void Main()
 	{
+		var schematic = new Schematic.Schematic(new NbtFile("pokecenter.nbt"));
 		Console.WriteLine("test");
 	}
 }
---
+++ Schematic/Schematic.cs
@@ -0,0 +1,40 @@
+using fNbt;
+
+namespace schematic_to_lost_cities.Schematic;
+
+public class Schematic
+{
+	public readonly NbtCompound[,,] Blocks;
+	private NbtCompound[] _palette;
+	
+	public Schematic(NbtFile file)
+	{
+		var size = file.RootTag.Get<NbtList>("size");
+		
+		var x = size.Get<NbtInt>(0).IntValue;
+		var y = size.Get<NbtInt>(1).IntValue;
+		var z = size.Get<NbtInt>(2).IntValue;
+
+		Blocks = new NbtCompound[y, x, z];
+		_palette = file.RootTag.Get<NbtList>("palette").ToArray<NbtCompound>();
+		
+		var blocks = file.RootTag.Get<NbtList>("blocks");
+
+		foreach (var block in blocks)
+		{
+			if (block is not NbtCompound compound)
+			{
+				continue;
+			}
+			
+			var pos = compound.Get<NbtList>("pos");
+			var blockX = pos.Get<NbtInt>(0).IntValue;
+			var blockY = pos.Get<NbtInt>(1).IntValue;
+			var blockZ = pos.Get<NbtInt>(2).IntValue;
+
+			var state = compound.Get<NbtInt>("state").IntValue;
+
+			Blocks[blockY, blockX, blockZ] = _palette[state];
+		}
+	}
+}
